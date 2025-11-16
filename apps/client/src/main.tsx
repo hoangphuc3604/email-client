ca086d15c877e571f424d56bf89d8deaafa5d08e
vow130504 <22120344@student.hcmus.edu.vn>
2025-11-17 02:03:06 +0700

    Home page

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css' // Đảm bảo import file index.css
+import App from './App' // Sửa từ App.tsx thành App
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
