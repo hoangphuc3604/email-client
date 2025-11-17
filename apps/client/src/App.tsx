ff5639eabbd00bd98fea182610ebd8afaafdf64d
vow130504 <22120344@student.hcmus.edu.vn>
2025-11-17 15:53:15 +0700

    Update Dashboard file

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,7 +3,7 @@ import Preloader from "./components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
+import Dashboard from "./components/Dashboard/Dashboard";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 
@@ -19,6 +19,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { GrDashboard } from "react-icons/gr";
 
 function App() {
   const [load, updateLoad] = useState(true);
@@ -39,7 +40,7 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/login" element={<Login />} />
