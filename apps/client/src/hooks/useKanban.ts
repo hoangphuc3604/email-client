c83b845de23cf1ac53664128395ee89e81154641
22120325 <nhattan107a@gmail.com>
2025-12-22 22:52:22 +0700

    fix: fix typescript errors

--- apps/client/src/hooks/useKanban.ts
+++ apps/client/src/hooks/useKanban.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import mailApi from '../api/mail'
-import { loadColumnConfig, getGmailLabelForColumn, type KanbanColumnConfig } from '../utils/kanbanConfig'
+import { loadColumnConfig } from '../utils/kanbanConfig'
 
 export type KanbanColumnId = string
 
